bbb501e53b48c30dfe2ddadd77a96ee2686215eb
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-04 19:48:38 +0600

    Add-Add-bootstrap-UI-FrameworkBootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,34 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
     return (
         <BrowserRouter>
-            <div>
+            <div className="d-flex flex-column site-container">
                 <header>
-                    <header>
-                        <Link to="/">amazona</Link>
-                    </header>
-                    <main>
-                        <Routes>
-                            <Route path='/product/:slug' element={<ProductScreen />} />
-                            <Route path='/' element={<HomeScreen />} />
-                        </Routes>
-                    </main>
+                    <Navbar bg='dark' variant="dark">
+                        <Container>
+                            <LinkContainer to="/">
+                                <Navbar.Brand>amazona</Navbar.Brand>
+                            </LinkContainer>
+                        </Container>
+                    </Navbar>
                 </header>
+                <main>
+                    <Container>
+                        <Routes>
+                            <Route path="/product/:slug" element={<ProductScreen />} />
+                            <Route path="/" element={<HomeScreen />} />
+                        </Routes>
+                    </Container>
+                </main>
+                <footer>
+                    <div className="text-center">All rights reserved</div>
+                </footer>
             </div>
         </BrowserRouter>
     );
