f7569250e4a39c8f3c3b8b4848b392c9c9cc6411
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-05 16:24:12 +0600

    Complete-Add-To-Cart

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,16 @@ app.get('/api/products/slug/:slug', (req, res) => {
     }
 });
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product not found.' });
+    }
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`);
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -51,9 +51,20 @@ const ProductScreen = () => {
     }, [slug]);
 
     const { state, dispatch: ctxDispatch } = useContext(Store);
+    const { cart } = state;
 
-    const addToCartHandler = () => {
-        ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } })
+    const addToCartHandler = async () => {
+        const existItem = cart.cartItems?.find((x => x._id === product._id));
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+
+        const { data } = await axios.get(`/api/products/${product._id}`);
+
+        if (data.countInStock < quantity) {
+            window.alert('Sorry! Product is out of stock.');
+            return;
+        }
+
+        ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
     };
 
     return (
