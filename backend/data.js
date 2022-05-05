9fcf2921d2bbaece74b9175b433e5f211f6df383
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-06 01:40:48 +0600

    Seed-Sample-Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Admin',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'User',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+    ],
     products: [
         {
             // _id: '1',
--- backend/routers/seedRouters.js
+++ backend/routers/seedRouters.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import data from '../data.js';
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
     await Product.deleteMany({});
     const createProducts = await Product.insertMany(data.products);
-    res.send({ createProducts });
+    await User.deleteMany({});
+    const createUsers = await User.insertMany(data.users);
+    res.send({ createProducts, createUsers });
 });
 
 export default seedRouter;
