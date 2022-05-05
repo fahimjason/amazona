1a28efa5647407114393c1b51dcd32e32f0c43c0
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-05 22:32:10 +0600

    Create-Signin-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
     const { state } = useContext(Store);
@@ -41,6 +42,7 @@ function App() {
                         <Routes>
                             <Route path="/product/:slug" element={<ProductScreen />} />
                             <Route path="/cart" element={<CartScreen />} />
+                            <Route path="/signin" element={<SigninScreen />} />
                             <Route path="/" element={<HomeScreen />} />
                         </Routes>
                     </Container>
