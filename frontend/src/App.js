34fa3d1556e2bfc63f7c17fdc382cd6903c577cf
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-07 23:32:36 +0600

    Create-Place-Order-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
     const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -82,6 +83,7 @@ function App() {
                             <Route path="/signup" element={<SignupScreen />} />
                             <Route path="/shipping" element={<ShippingAddressScreen />} />
                             <Route path="/payment" element={<PaymentMethodScreen />} />
+                            <Route path="/placeorder" element={<PlaceOrderScreen />} />
                             <Route path="/" element={<HomeScreen />} />
                         </Routes>
                     </Container>
