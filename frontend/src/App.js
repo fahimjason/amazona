6e4607a04c27c4790137b18961f1b7e1ee0024f0
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-07 12:49:36 +0600

    Create-Shipping-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
     const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -21,6 +22,7 @@ function App() {
     const signoutHandler = () => {
         ctxDispatch({ type: 'USER_SIGNOUT' });
         localStorage.removeItem('userInfo');
+        localStorage.removeItem('shippingAddress');
     };
 
     return (
@@ -74,6 +76,7 @@ function App() {
                             <Route path="/product/:slug" element={<ProductScreen />} />
                             <Route path="/cart" element={<CartScreen />} />
                             <Route path="/signin" element={<SigninScreen />} />
+                            <Route path="/shipping" element={<ShippingAddressScreen />} />
                             <Route path="/" element={<HomeScreen />} />
                         </Routes>
                     </Container>
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -9,7 +9,10 @@ const initialState = {
     cart: {
         cartItems: localStorage.getItem('cartItems')
             ? JSON.parse(localStorage.getItem('cartItems'))
-            : []
+            : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+            ? JSON.parse(localStorage.getItem('shippingAddress'))
+            : {}
     }
 };
 
@@ -40,7 +43,16 @@ const reducer = (state, action) => {
         case 'USER_SIGNIN':
             return { ...state, userInfo: action.payload };
         case 'USER_SIGNOUT':
-            return { ...state, userInfo: null };
+            return {
+                ...state,
+                userInfo: null,
+                cart: {
+                    cartItems: [],
+                    shippingAddress: {},
+                }
+            };
+        case 'SAVE_SHIPPING_ADDRESS':
+            return { ...state, cart: { ...state.cart, shippingAddress: action.payload } };
 
         default:
             return state;
