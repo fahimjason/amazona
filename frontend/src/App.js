df368e6cb800a945e4e04777deb584b53b18b4ea
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-11 23:42:40 +0600

    Create-Admin-Menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,6 +25,9 @@ import axios from 'axios';
 import { getError } from './utils';
 import SearchBox from './components/SearchBox';
 import SearchScreen from './screens/SearchScreen';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminRoute from './components/AdminRoute';
+import DashboardScreen from './screens/DashboardScreen';
 
 function App() {
     const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -108,6 +111,22 @@ function App() {
                                             Sign In
                                         </Link>
                                     )}
+                                    {userInfo && userInfo.isAdmin && (
+                                        <NavDropdown title="Admin" id="admin-nav-dropdown">
+                                            <LinkContainer to="/admin/dashboard">
+                                                <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                                            </LinkContainer>
+                                            <LinkContainer to="/admin/productlist">
+                                                <NavDropdown.Item>Products</NavDropdown.Item>
+                                            </LinkContainer>
+                                            <LinkContainer to="/admin/orderlist">
+                                                <NavDropdown.Item>Orders</NavDropdown.Item>
+                                            </LinkContainer>
+                                            <LinkContainer to="/admin/userlist">
+                                                <NavDropdown.Item>Users</NavDropdown.Item>
+                                            </LinkContainer>
+                                        </NavDropdown>
+                                    )}
                                 </Nav>
                             </Navbar.Collapse>
                         </Container>
@@ -144,12 +163,13 @@ function App() {
                             <Route path="/search" element={<SearchScreen />} />
                             <Route path="/signin" element={<SigninScreen />} />
                             <Route path="/signup" element={<SignupScreen />} />
-                            <Route path="/profile" element={<ProfileScreen />} />
+                            <Route path="/profile" element={<ProtectedRoute> <ProfileScreen /> </ProtectedRoute>} />
                             <Route path="/shipping" element={<ShippingAddressScreen />} />
                             <Route path="/payment" element={<PaymentMethodScreen />} />
                             <Route path="/placeorder" element={<PlaceOrderScreen />} />
-                            <Route path="/order/:id" element={<OrderScreen />} />
-                            <Route path="/orderhistory" element={<OrderHistoryScreen />} />
+                            <Route path="/order/:id" element={<ProtectedRoute> <OrderScreen /> </ProtectedRoute>} />
+                            <Route path="/orderhistory" element={<ProtectedRoute> <OrderHistoryScreen /> </ProtectedRoute>} />
+                            <Route path="/admin/dashboard" element={<AdminRoute> <DashboardScreen /></AdminRoute>} />
                             <Route path="/" element={<HomeScreen />} />
                         </Routes>
                     </Container>
---
+++ frontend/src/screens/DashboardScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashboardScreen() {
+    return (
+        <div>
+            Dashboard Screen
+        </div>
+    )
+}
